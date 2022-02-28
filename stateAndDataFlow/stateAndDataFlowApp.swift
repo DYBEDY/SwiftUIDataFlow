712bd397e86e453afdd2d601055bb039a25215e8
DYBEDY <90396626+DYBEDY@users.noreply.github.com>
2022-02-28 17:39:17 +0300

    Initial Commit

---
+++ stateAndDataFlow/stateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  stateAndDataFlowApp.swift
+//  stateAndDataFlow
+//
+//  Created by Roman on 28.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct stateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
